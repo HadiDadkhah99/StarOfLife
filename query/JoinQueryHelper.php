fb86d88b2fda5f9f0e9c91a4d23d308c5606b50d
hadidadkhah <hadidadkhah99@gmail.com>
2021-07-11 13:49:48 +0430

    commit

---
+++ query/JoinQueryHelper.php
@@ -0,0 +1,145 @@
+<?php
+
+
+class JoinQueryHelper
+{
+
+
+    /** @var $whereQuery WhereQuery */
+    private $whereQuery;
+    /** @var $joinQuery string */
+    private $joinQuery = "";
+
+
+    public function __construct(WhereQuery $whereQuery)
+    {
+        $this->whereQuery = $whereQuery;
+    }
+
+
+    public function join(DataModel $dataModel, string $as = null): JoinQueryHelper
+    {
+        //add to joined tables
+        $temp = $this->whereQuery->getJoinedTables();
+        $temp[] = $dataModel->name();
+        $this->whereQuery->setJoinedTables($temp);
+
+        $this->joinQuery .= empty($as) ? " JOIN {$dataModel->getTableName()} ON " : " JOIN {$dataModel->getTableName()} as $as ON ";
+
+        return $this;
+    }
+
+    public function leftJoin(DataModel $dataModel, string $as = null): JoinQueryHelper
+    {
+        //add to joined tables
+        $temp = $this->whereQuery->getJoinedTables();
+        $temp[] = $dataModel->name();
+        $this->whereQuery->setJoinedTables($temp);
+        
+        $this->joinQuery .= empty($as) ? " LEFT JOIN {$dataModel->getTableName()} ON " : " JOIN {$dataModel->getTableName()} as $as ON ";
+
+        return $this;
+    }
+
+    /**
+     * @param string $var
+     * @param $value mixed
+     * @throws Exception
+     */
+    public function onEqual(string $var, $value, bool $not = false): JoinQueryHelper
+    {
+        if (!strpos($var, "."))
+            throw new Exception("Err in set column name: please set var name like ( test_table.name )");
+
+        //compare column with column
+        if (is_string($value) && strpos($value, ".")) {
+            $this->joinQuery .= $not ? " NOT $var=$value " : " $var = $value ";
+        } //compare column with mixed value
+        else {
+            $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+            $this->joinQuery .= $not ? " NOT $var=:$e " : " $var=:$e ";
+            $vars = $this->whereQuery->getVars();
+            $vars[":$e"] = $value;
+            $this->whereQuery->setVars($vars);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $var
+     * @param $value mixed
+     * @throws Exception
+     */
+    public function onNotEqual(string $var, $value): void
+    {
+        $this->onEqual($var, $value, true);
+    }
+
+    /**
+     * @param string $var
+     * @param $value mixed
+     * @throws Exception
+     */
+    public function onLessThan(string $var, $value, bool $orEqual): JoinQueryHelper
+    {
+        if (!strpos($var, "."))
+            throw new Exception("Err in set column name: please set var name like ( test_table.name )");
+
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+
+        $this->joinQuery .= $orEqual ? "  $var<=:$e " : "  $var<:$e ";
+        $vars = $this->whereQuery->getVars();
+        $vars[":$e"] = $value;
+        $this->whereQuery->setVars($vars);
+
+        return $this;
+    }
+
+
+    /**
+     * @param string $var
+     * @param $value mixed
+     * @throws Exception
+     */
+    public function onGreaterThan(string $var, $value, bool $orEqual): JoinQueryHelper
+    {
+        if (!strpos($var, "."))
+            throw new Exception("Err in set column name: please set var name like ( test_table.name )");
+
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+
+        $this->joinQuery .= $orEqual ? "  $var>=:$e " : "  $var>:$e ";
+        $vars = $this->whereQuery->getVars();
+        $vars[":$e"] = $value;
+        $this->whereQuery->setVars($vars);
+
+        return $this;
+    }
+
+
+    public function not(): JoinQueryHelper
+    {
+
+
+        $this->joinQuery .= " NOT ";
+
+        return $this;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function commitJoin(): WhereQuery
+    {
+        if (!(strpos($this->joinQuery, "=") or strpos($this->joinQuery, ">") or strpos($this->joinQuery, "<")))
+            throw new Exception("Err in join query: You must set at last one check statement like ( onEqual() )");
+
+        $this->whereQuery->setWhereQuery($this->joinQuery . $this->whereQuery->getWhereQuery());
+
+        return $this->whereQuery;
+    }
+
+
+}
---
+++ query/WhereQuery.php
@@ -0,0 +1,254 @@
+<?php
+require_once dirname(__DIR__) . '/query/WhereQueryController.php';
+require_once dirname(__DIR__) . '/query/JoinQueryHelper.php';
+
+
+class WhereQuery
+{
+
+    /** @var $checkUseWhere bool */
+    private $checkUseWhere = false;
+    /** @var $whereQuery string */
+    private $whereQuery = "";
+    /** @var $vars array */
+    private $vars = [];
+    /** @var $joinedTables array */
+    private $joinedTables = [];
+
+    //controller
+    /** @var $controller WhereQueryController */
+    private $controller;
+    //join helper
+    /** @var $joinQueryHelper JoinQueryHelper */
+    private $joinQueryHelper;
+
+
+    /**
+     * WhereQuery constructor.
+     */
+    public function __construct()
+    {
+        $this->controller = new WhereQueryController($this);
+        $this->joinQueryHelper = new JoinQueryHelper($this);
+    }
+
+
+    /**
+     * @param mixed $value
+     */
+    public function equal(string $var, $value): WhereQuery
+    {
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+        $this->whereQuery .= !$this->checkUseWhere ? " WHERE " : "";
+        $this->whereQuery .= " $var=:$e ";
+        $this->vars[":$e"] = $value;
+
+        $this->checkUseWhere = true;
+        return $this;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function notEqual(string $var, $value): WhereQuery
+    {
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+        $this->whereQuery .= !$this->checkUseWhere ? " WHERE " : "";
+        $this->whereQuery .= " NOT $var=:$e ";
+        $this->vars[":$e"] = $value;
+
+        $this->checkUseWhere = true;
+
+        return $this;
+    }
+
+
+    /**
+     * @param bool $orEqual
+     * @param int|float $value
+     */
+    public function greatThan(string $var, $value, bool $orEqual = false): WhereQuery
+    {
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+        $this->whereQuery .= !$this->checkUseWhere ? " WHERE " : "";
+        $this->whereQuery .= $orEqual ? " $var>=:$e " : " $var>:$e ";
+        $this->vars[":$e"] = $value;
+
+        $this->checkUseWhere = true;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $orEqual
+     * @param int|float $value
+     */
+    public function lessThan(string $var, $value, bool $orEqual = false): WhereQuery
+    {
+        $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+        $this->whereQuery .= !$this->checkUseWhere ? " WHERE " : "";
+        $this->whereQuery .= $orEqual ? " $var<=:$e " : " $var<:$e ";
+        $this->vars[":$e"] = $value;
+
+        $this->checkUseWhere = true;
+
+        return $this;
+    }
+
+
+    public function like(string $var, string $like): WhereQuery
+    {
+        //control
+        $this->controller->likeControl();
+        $this->whereQuery .= !$this->checkUseWhere ? " WHERE " : "";
+        $this->whereQuery .= " $var LIKE '$like' ";
+
+        $this->checkUseWhere = true;
+
+        return $this;
+    }
+
+
+    public function and(): WhereQuery
+    {
+        $this->whereQuery .= " AND ";
+
+        return $this;
+    }
+
+    public function or(): WhereQuery
+    {
+        $this->whereQuery .= " OR ";
+
+        return $this;
+    }
+
+    public function not(): WhereQuery
+    {
+        $this->whereQuery .= " NOT ";
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function orderBy(string $var, bool $asc = true): WhereQuery
+    {
+        //control
+        $this->controller->orderByControl();
+
+        // $e = strpos($var, ".") ? explode(".", $var)[1] : $var;
+        $e = $var;
+        $AD = $asc ? "ASC" : "DESC";
+        $this->whereQuery .= " ORDER BY $e $AD ";
+
+
+        return $this;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function customOrderBy(string $orderBy): WhereQuery
+    {
+        //control
+        $this->controller->orderByControl();
+        $this->whereQuery .= " $orderBy ";
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function limit(int $count): WhereQuery
+    {
+        //control
+        $this->controller->limitControl();
+
+        $this->whereQuery .= " LIMIT $count ";
+
+
+        return $this;
+    }
+
+
+    /**
+     * page number start from 0
+     * $count is count of page items
+     * @throws Exception
+     */
+    public function page(int $page, int $count): WhereQuery
+    {
+        //control
+        $this->controller->pageControl();
+
+        $this->whereQuery .= " LIMIT " . ($page * $count) . " , $count ";
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $as
+     * @throws Exception
+     */
+    public function join(DataModel $dataModel, string $as = null): JoinQueryHelper
+    {
+        return $this->joinQueryHelper->join($dataModel, $as);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function leftJoin(DataModel $dataModel, string $as = null): JoinQueryHelper
+    {
+
+        return $this->joinQueryHelper->leftJoin($dataModel, $as);
+    }
+
+    public function setWhereQuery(string $whereQuery): void
+    {
+        $this->whereQuery = $whereQuery;
+    }
+
+
+    public function getWhereQuery(): string
+    {
+        return $this->whereQuery;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getVars(): array
+    {
+        return $this->vars;
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setVars(array $vars): void
+    {
+        $this->vars = $vars;
+    }
+
+    /**
+     * @return array
+     */
+    public function getJoinedTables(): array
+    {
+        return $this->joinedTables;
+    }
+
+    /**
+     * @param array $joinedTables
+     */
+    public function setJoinedTables(array $joinedTables): void
+    {
+        $this->joinedTables = $joinedTables;
+    }
+
+}
+
